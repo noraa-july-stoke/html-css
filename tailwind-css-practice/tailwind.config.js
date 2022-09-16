18703eb49e48ed5de2e18a476f9ee3010f2f7816
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-09-16 18:43:30 -0400

    ok

--- tailwind-css-practice/tailwind.config.js
+++ tailwind-css-practice/tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      // gridTemplateColumns: {
+      //   'hellomeow' : '20% 80% 20%'
+      // },
+    },
   },
   plugins: [],
 }
+
+
+// tailwind.config.js
+// module.exports = {
+//   theme: {
+//     extend: {
+//       gridTemplateColumns: {
+//         // Simple 16 column grid
+//         '16': 'repeat(16, minmax(0, 1fr))',
+
+//         // Complex site-specific column configuration
+//         'footer': '200px minmax(900px, 1fr) 100px',
+//       }
+//     }
+//   }
+// }
