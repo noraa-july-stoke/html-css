0fd4155f129eba174b1ca02923365ae3c437b6c9
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-09-25 21:40:17 -0400

    node server

--- server-practice/app.js
+++ server-practice/app.js
@@ -1,10 +1,19 @@
 const http = require("http");
-
-const port = 3000;
+const port = 5000;
+const fs = require('fs')
 
 const server = http.createServer(function(req, res) {
 
-
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', (error, data) => {
+        if (error) {
+            res.writeHead(404);
+            res.write('error: File Not Found');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
 
 });
 
